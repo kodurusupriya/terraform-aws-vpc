14ade23a8eaa7f8b816097f7f61d77a95b10f02b
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-27 22:19:32 -0500

    actions

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ resource "aws_internet_gateway" "main" {
     vpc_id = aws_vpc.main.id  # vpc association
     tags = local.igw_final_tags
 }
+
+/* #public subnet
+resource "aws_subnet" "public" {
+    count = length(var.public_subnet_cidrs)
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.public_subnet_cidrs[count.index]
+    tags = {
+        name = "main"
+    } */
+/* } */
