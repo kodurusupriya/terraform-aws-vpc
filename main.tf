d33aa2a4582778c979bbcb52c198a959cb1b7582
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-27 21:53:59 -0500

    actions

--- main.tf
+++ main.tf
@@ -3,11 +3,10 @@ resource "aws_vpc" "main" {
     instance_tenancy = "default"
     enable_dns_hostnames = true
 
-    tags = merge(
-        local.common_tags,
-        {
-            name = "${var.project}-${var.environment}"
-        },
-        var.vpc_tags
-    ) 
+    tags = local.vpc_final_tags
+    }
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id  # vpc association
+    tags = local.igw_final_tags
 }
